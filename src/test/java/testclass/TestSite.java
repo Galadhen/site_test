3a6bd956eefa64af81d746778c7f4f4b489bb941
Anton Novikov <novikov@tochkak.ru>
2017-09-20 15:19:43 +0300

    hotfix

--- src/test/java/testclass/TestSite.java
+++ src/test/java/testclass/TestSite.java
@@ -7,12 +7,10 @@ import org.junit.Test;
 import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static testclass.util.checkHall;
 import static testclass.util.timeout;
 import static testclass.util.useChromeDriver;
 
@@ -28,77 +26,66 @@ public class TestSite {
         open("https://kinowidget.kinoplan.ru/392/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
     @Test
     public void letsTestMayak() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/1280/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+       checkHall();
     }
     @Test
     public void letsTestSaratov() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/2086/");
         timeout(800);
-        assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        assertTrue("Today all over", $(".seance-item").exists());
+        checkHall();
     }
     @Test
     public void letsTestKinopolis() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/3963/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
-    }
+        checkHall();}
     @Test
     public void letsTestLomonosov() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/4108/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
     @Test
     public void letsTestKirov() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/2696/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
     @Test
     public void letsTestSovremennik() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/160/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
     @Test
     public void letsTestMihaylovsk() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/4140/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
     @Test
     public void letsTestJalilN() throws MalformedURLException {
         open("https://kinowidget.kinoplan.ru/237/");
         timeout(800);
         assertTrue("Today all over",$(".seance-item").exists());
-        $$(".seance-item").get(0).click();
-        assertFalse("Houston, you have a problem", $(".payment-responce__title").exists());
+        checkHall();
     }
 
     @After
     public void tearDown() throws Exception {
-        getWebDriver().quit();
         closeWebDriver();
     }
 }
--- src/test/java/testclass/util.java
+++ src/test/java/testclass/util.java
@@ -3,15 +3,21 @@ package testclass;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.net.MalformedURLException;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class util {
 
     public static void useChromeDriver() throws MalformedURLException{
-     ChromeDriverManager.getInstance().setup();
+        ChromeDriverManager.getInstance().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("no-sandbox","headless","disable-gpu");
         WebDriver browser = new ChromeDriver(options);
@@ -24,6 +30,18 @@ public class util {
             Thread.sleep(millis);
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+    public static void checkHall() {
+        $$(".seance-item").get(0).click();
+        timeout(500);
+        if ($(By.className("spinner")).exists()){
+            timeout(5000);
+            if ($(".spinner").exists()){
+                fail("Houston you have a problem");}
+        }
+        else {
+            assertTrue("Houston, you have a problem", !$(".payment-responce__title").exists());
         }
     }
 }
